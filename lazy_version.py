18814a927d285d8b69c40ef2c32dba4e6766140c
subi26 <mydearsubi@gmail.com>
2023-09-30 11:58:30 +0530

    Update and rename basic_functional_version.py to lazy_version.py
    Added Date Time Filter In Lazy Version

--- lazy_version.py
+++ lazy_version.py
@@ -4,6 +4,7 @@ import asyncio
 from bs4 import BeautifulSoup
 from pprint import pprint
 import urllib.parse
+from datetime import datetime
 
 # Define the list of URLs pointing to JPEG images
 image_urls = []
@@ -12,12 +13,22 @@ image_data = {}
 
 href_urls = []
 
+host = 'http://192.168.1.2:12345'
 # Define the URL to fetch HTML content from
-# url = 'http://192.168.1.5:12345/Android%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia%2FWhatsApp+Images'
-url = 'http://192.168.1.5:12345/inShare%2Ffiles%2FWhatsApp+Images'
+url = host+'/Android%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia%2FWhatsApp+Images'
+# url = 'http://192.168.1.5:12345/inShare%2Ffiles%2FWhatsApp+Images'
+
+print(f"Fetching URL -> {url}")
 
 # Send a GET request to the URL using grequests
 response = grequests.get(url).send().response
+
+start_date = datetime.strptime("6/01/2023", "%m/%d/%Y")
+end_date = datetime.strptime("9/29/2023", "%m/%d/%Y")
+
+total_links = 0
+filtered_iinks = 0
+skipped_links = 0
 
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
@@ -28,13 +39,28 @@ if response.status_code == 200:
     # Find all <a> tags
     a_tags = soup.find_all('a')
 
+    print(f"{len(a_tags)} Links Found")
+
     # Extract href URLs from the <a> tags
     for a in a_tags:
         if 'Private' in a.text or 'Sent' in a.text or '..' in a.text:
             continue
-        href_urls.append(f"http://192.168.1.5:12345/{a.get('href')}")
+
+        try:
+            date_components = a.text.split('-')
+            date = date_components[1]  # Format: '29/09/2023'
+            file_date = datetime.strptime(date, "%Y%m%d")
+            if start_date <= file_date <= end_date:
+                href_urls.append(f"{host}/{a.get('href')}")
+                filtered_iinks += 1
+            else:
+                skipped_links +=1
+        except:
+            pass
 
     print(f"{len(href_urls)} Images Found")
+    print(f"{skipped_links} Links Skipped")
+
 else:
     print(f"Failed to fetch HTML content from {url}. Status code: {response.status_code}")
 
@@ -108,6 +134,6 @@ for batch_urls in url_batches:
 
 # Close the event loop
 loop.close()
-
-print(f"Success : {s_count} && Failed : {f_count}")
 pprint(failed_urls)
+print(f"Success : {s_count} && Failed : {f_count}")
+
